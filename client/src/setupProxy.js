cdf04eecd7b5f1eedb777c5755d40ceb494513e6
Sriram <rammurthy434@gmail.com>
2021-02-27 16:40:40 +0000

    created build

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 module.exports = function(App) {
   App.use(
     createProxyMiddleware(["/api", , "/otherApi"], 
-		  { target: "https://sriram-fntgn.run-ap-south1.goorm.io/",changeOrigin: true })
+		  { target: "https://warbler-msg-server.herokuapp.com/",changeOrigin: true })
 	  
   );
 };
